f5df30e7cce86f73c5e6f5eddc628fbd928aa061
dalerank <dalerankn8@gmail.com>
2024-11-02 23:08:11 +0100

    ui: add class for warship wharf window info

---
+++ src/window/window_warshipwharf_info.cpp
@@ -0,0 +1,49 @@
+#include "window_building_info.h"
+
+#include "building/building_warship_wharf.h"
+#include "city/object_info.h"
+#include "city/city_resource.h"
+#include "input/input.h"
+#include "graphics/window.h"
+#include "building/common.h"
+#include "figure/figure.h"
+
+struct warshipwharf_info_window : public building_info_window_t<warshipwharf_info_window> {
+    using widget::load;
+    virtual void load(archive arch, pcstr section) override {
+        widget::load(arch, section);
+    }
+
+    virtual void init(object_info &c) override;
+    virtual bool check(object_info &c) override {
+        return c.building_get()->type == BUILDING_WARSHIP_WHARF;
+    }
+};
+
+warshipwharf_info_window warshipwharf_infow;
+
+void warshipwharf_info_window::init(object_info &c) {
+    building_info_window::init(c);
+
+    building *b = c.building_get();
+
+    std::pair<int, int> reason = { c.group_id, 0 };
+    if (!c.has_road_access) {
+        reason = { 69, 25 };
+    } else if (!b->get_figure(BUILDING_SLOT_BOAT)->is_valid()) {
+        reason = { c.group_id, 2 };
+    } else {
+        figure *boat = b->get_figure(BUILDING_SLOT_BOAT);
+        switch (boat->action_state) {
+        case FIGURE_ACTION_203_WARSHIP_MOORED: reason.second = 2; break;
+        case FIGURE_ACTION_204_WARSHIP_ATTACK: reason.second = 3; break;
+        default: reason.second = 8; break;
+        }
+    }
+
+    ui["resource_img"].image(RESOURCE_FISH);
+    ui["warning_text"] = ui::str(reason.first, reason.second);
+    ui["storage_desc"].text_var("Stored weapons %d", b->stored_full_amount);
+
+    fill_employment_details(c);
+}
