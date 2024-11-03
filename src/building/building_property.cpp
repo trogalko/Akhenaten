addc060bbc4390e26fea7d0de548f582312b006e
dalerank <dalerankn8@gmail.com>
2024-11-03 17:58:57 +0100

    tags: add new tags for buildings

--- src/building/building_property.cpp
+++ src/building/building_property.cpp
@@ -6,3 +6,7 @@ const id_property_t &tags() {
 }
 
 const xstring id_property_t::stored = "stored";
+const xstring id_property_t::building = "building";
+const xstring id_property_t::name = "name";
+const xstring id_property_t::house = "house";
+const xstring id_property_t::level_name = "level_name";
--- src/building/building_property.h
+++ src/building/building_property.h
@@ -4,6 +4,10 @@
 
 struct id_property_t {
     static const xstring stored;
+    static const xstring building;
+    static const xstring name;
+    static const xstring house;
+    static const xstring level_name;
 };
 
 const id_property_t &tags();
