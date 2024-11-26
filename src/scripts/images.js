d862d9f41ee2d7854a9951411c7b4fac2051dea1
dalerank <dalerankn8@gmail.com>
2024-11-26 23:23:42 +0100

    images: setup config for flax carts

--- src/scripts/images.js
+++ src/scripts/images.js
@@ -41,6 +41,7 @@ cart_images = [
 	{resource: RESOURCE_REEDS, pack:PACK_SPR_MAIN, id:99 },
 	{resource: RESOURCE_GOLD, pack:PACK_SPR_MAIN, id:98 },
 	{resource: RESOURCE_GEMS, pack:PACK_SPR_MAIN, id:95 },
+	{resource: RESOURCE_FLAX, pack:PACK_SPR_MAIN, id:93 },
 ]
 
 cart_offsets = [
