15ef9ad3b7de2477909743907b27a778ecedf82c
dalerank <dalerankn8@gmail.com>
2024-11-30 22:18:16 +0100

    game: moved creating logic for burning_ruin from common space to class

--- src/building/destruction.cpp
+++ src/building/destruction.cpp
@@ -20,17 +20,11 @@
 #include <core/random.h>
 #include <string.h>
 
-const vec2i destroy_on_fire_tiles[] = {         {0, 0},
-                                        {1, 0}, {1, 1}, {0, 1}, 
-                                {2, 0}, {2, 1}, {2, 2}, {1, 2}, {0, 2}, 
-                        {3, 0}, {3, 1}, {3, 2}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, 
-                {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, 
-        {5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}};
-
 static void destroy_on_fire(building* b, bool plagued) {
     game_undo_disable();
     b->fire_risk = 0;
     b->damage_risk = 0;
+
     if (b->house_size && b->house_population > 0) {
         city_population_remove_home_removed(b->house_population);
     }
@@ -48,55 +42,19 @@ static void destroy_on_fire(building* b, bool plagued) {
         waterside_building = 1;
     }
 
-    int num_tiles;
-    if (b->size >= 2 && b->size <= 5) {
-        num_tiles = b->size * b->size;
-    } else {
-        if (b->house_size > 1) {
-            num_tiles = b->house_size * b->house_size;
-        } else {
-            num_tiles = 0;
-        }
-    }
-
     map_building_tiles_remove(b->id, b->tile);
     unsigned int rand_int = random_short();
-    if (map_terrain_is(b->tile, TERRAIN_WATER)) {
-        b->state = BUILDING_STATE_DELETED_BY_GAME;
-    } else {
-        b->type = BUILDING_BURNING_RUIN;
-        b->remove_figure(3);
-        b->tax_income_or_storage = 0;
-        b->fire_duration = (b->map_random_7bit & 7) + 1;
-        b->fire_proof = 1;
-        b->size = 1;
-        b->has_plague = plagued;
-        memset(&b->data, 0, sizeof(building::data));
 
-        // FIXME: possible can't render image & fire animation
-        unsigned char random = rand_int % 4;
-        rand_int *= rand_int;
-        int image_id = image_id_from_group(GROUP_TERRAIN_RUBBLE_GENERAL) + 9 * random;
-        map_building_tiles_add(b->id, b->tile, 1, image_id, TERRAIN_BUILDING);
-    }
-
-    for (int tile = 1; tile < num_tiles; tile++) {
-        tile2i shifted = b->tile.shifted(destroy_on_fire_tiles[tile].x, destroy_on_fire_tiles[tile].y);
-        if (map_terrain_is(shifted, TERRAIN_WATER)) {
-            continue;
+    grid_area varea = map_grid_get_area(b->tile, b->size, 0);
+    map_grid_area_foreach(varea, [plagued] (tile2i tile) {
+        if (map_terrain_is(tile, TERRAIN_WATER)) {
+            return;
         }
 
         // FIXME: possible can't render image & fire animation
-        building* ruin = building_create(BUILDING_BURNING_RUIN, shifted, 0);
-        unsigned char random = rand_int % 4;
-        rand_int *= rand_int;
-        int image_id = image_id_from_group(GROUP_TERRAIN_RUBBLE_GENERAL) + 9 * random;
-        map_building_tiles_add(ruin->id, ruin->tile, 1, image_id, TERRAIN_BUILDING);
-        ruin->fire_duration = (ruin->map_random_7bit & 7) + 1;
-        b->remove_figure(3);
-        ruin->fire_proof = 1;
+        building *ruin = building_create(BUILDING_BURNING_RUIN, tile, 0);
         ruin->has_plague = plagued;
-    }
+    });
 
     if (waterside_building) {
         map_routing_update_water();
