82c1ccd09ab073431f9f03119ecd479614d47f2b
dalerank <dalerankn8@gmail.com>
2024-11-30 22:19:28 +0100

    services: improved logic for service coverage

--- src/figure/service.h
+++ src/figure/service.h
@@ -16,10 +16,8 @@ inline int figure_provide_service(tile2i tile, figure* f, int &data, T callback)
         int building_id = map_building_at(grid_offset);
         if (building_id) {
             building *b = building_get(building_id);
-            if (b->house_size && b->house_population > 0) {
-                callback(b, f, data);
-                serviced++;
-            }
+            callback(b, f, data);
+            serviced++;
         }
     });
     return serviced;
