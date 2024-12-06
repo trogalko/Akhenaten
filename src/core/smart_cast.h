ccf7af21a046e2950d07a10a540803166daec4a4
dalerank <dalerankn8@gmail.com>
2024-12-07 00:53:59 +0100

    engine: redesigned smart_cast function

--- src/core/smart_cast.h
+++ src/core/smart_cast.h
@@ -1,101 +1,79 @@
 #pragma once
 
-#include <cstddef>
+#include <utility>
 
-// Magic value used to indicate that there is no cache entry for this particular
-// conversion yet. Should be a fairly high value to avoid collisions.
-constexpr ptrdiff_t DCAST_NO_OFFSET = 0x7FFFFFFFFFFFFFFFL;
+template <typename dest_type>
+struct smart_cast_dcast_helper;
 
-#include <memory>
-#include <assert.h>
+#define IMPL_GEN_SMART_CAST(type, dcastfun)       \
+    template <>                                   \
+    struct smart_cast_dcast_helper<type*> {       \
+        template <typename src_type>              \
+        static inline type* cast(src_type& src) { \
+            return src.dcastfun();                \
+        }                                         \
+    };
 
-using class_obj_ptr = const uintptr_t*;
-using v_table_ptr = const uintptr_t*;
+#define GENERATE_SMART_CAST(type) IMPL_GEN_SMART_CAST(type, dcast_##type)
+#define GENERATE_SMART_CAST_CUSTOM(type, custom) IMPL_GEN_SMART_CAST(type, dcast_##custom)
+#define GENERATE_SMART_CAST_NS(ns, type) IMPL_GEN_SMART_CAST(ns::type, dcast_##ns##_##type)
+#define GENERATE_SMART_CAST_NS_SHORT(ns, type) IMPL_GEN_SMART_CAST(ns::type, dcast_##type)
 
-#ifdef _MSC_VER
-#define FORCELINLINE __forceinline
-#else
-#define FORCELINLINE inline
-#endif
+template <typename dest_type, typename src_type, bool dest_is_base_of_src>
+struct smart_cast_helper;
 
-template < typename T >
-FORCELINLINE v_table_ptr smart_cast_get_vtable(const T* ptr) {
-    // __vftable is at [ptr + 0]
-    return reinterpret_cast<v_table_ptr>(*reinterpret_cast<class_obj_ptr>(ptr));
-}
+template <typename same_type>
+struct smart_cast_helper<same_type *, same_type, false> {
+    // smart_cast to the same type - simply return pointer to src
+    static inline same_type *cast(same_type &src) { return &src; }
+};
 
-// Converts T&, const T&, volatile const T& etc to T, used for enable_if
-// Could also abuse std::decay for this.
-template < typename _Ty >
-using clean_type_t = typename std::remove_cv_t<std::remove_reference_t<std::remove_pointer_t< _Ty >>>;
+template <typename dest_type, typename src_type>
+struct smart_cast_helper<dest_type *, src_type, true> {
+    // smart_cast to base class - use implicit conversion from src_type * to dest_type *
+    static inline dest_type *cast(src_type &src) { return &src; }
+};
 
-// smart cast for T*
-// Should be nothrow but this decreases performance. In practice this is not
-// an issue.
-template < typename _To, typename _From >
-FORCELINLINE _To smart_cast(_From* ptr) {
-    // Not required, dynamic_cast already produces a compile-time error in this case
-    // static_assert(std::is_polymorphic<_From>::value, "Type is not polymorphic!");
+template <typename dest_type, typename src_type>
+struct smart_cast_helper<dest_type *, src_type, false> {
+    // smart_cast to some other dest_type which is not base of src_type - delegate to smart_cast_dcast_helper
+    static inline dest_type *cast(src_type &src) { return smart_cast_dcast_helper<dest_type *>::cast(src); }
+};
 
-    if (!ptr) {
+template <typename dest_type, typename src_type>
+inline typename std::enable_if_t<std::is_pointer_v<dest_type>, dest_type> smart_cast(src_type &&src) {
+    // src is a reference to a pointer or a reference to something that acts like a pointer;
+    // result of its dereferencing (*src) should be a reference to a value which is pointed to by src;
+    // decltype(*src) with its reference stripped off is the type of that (possibly, cv-qualified) pointee value
+    using src_value = std::remove_reference_t<decltype(*src)>;
+    using src_value_nocv = std::remove_cv_t<src_value> ;
+
+    // dest_type is definitely a raw pointer, so remove_pointer yields the type of its (possibly, cv-qualified) pointee value
+    using dest_value = std::remove_pointer_t<dest_type> ;
+    using dest_value_nocv = std::remove_cv_t<dest_value>;
+
+    // copy all cv-qualifiers from src_value to dest_value_nocv and declare a pointer to that resulting type
+    // which is the "least cv-qualified" type we can return (having taken src_value's qualifiers into account)
+    using dest_type_cv_protected = std::add_cv_t<dest_value_nocv> *;
+
+    // if src is not null, use a smart_cast_helper (which doesn't imply any cv-qualifiers at all) to perform the cast
+    // and get the "unprotected" result
+    if (!src) {
         return nullptr;
     }
 
-    thread_local static ptrdiff_t offset = DCAST_NO_OFFSET;
-    thread_local static v_table_ptr src_vtable_ptr;
-
-    v_table_ptr this_vtable = smart_cast_get_vtable(ptr);
-    if (offset != DCAST_NO_OFFSET && src_vtable_ptr == this_vtable) {
-        // In case we have a cache hit, casting the pointer is straightforward
-        char* new_ptr = reinterpret_cast<char*>(ptr) + offset;
-        return reinterpret_cast<_To>(new_ptr);
-    } else {
-        // Need to construct cache entry
-        auto result = dynamic_cast<_To>(ptr);
-        if (result == nullptr) {
-            return nullptr;
-        }
-
-        src_vtable_ptr = this_vtable;
-        offset = reinterpret_cast<const char*>(result) - reinterpret_cast<const char*>(ptr);
-        return result;
-    }
-};
-
-// const T*
-template < typename _To, typename _From >
-FORCELINLINE const _To smart_cast(const _From* ptr) {
-    _From* nonconst_ptr = const_cast<_From*>(ptr);
-    _To casted_ptr = smart_cast<_To>(nonconst_ptr);
-    return const_cast<const _To>(casted_ptr);
-};
-
-// T&
-template < typename _To, typename _From, typename = std::enable_if_t<!std::is_same<clean_type_t<_To>, clean_type_t<_From&>>::value>>
-FORCELINLINE _To smart_cast(_From& ref) {
-    using _ToPtr = std::add_pointer_t<std::remove_reference_t<_To>>;
-    auto casted_ptr = smart_cast<_ToPtr>(&ref);
-    assert(casted_ptr);
-
-    return *casted_ptr;
-};
-
-// const T&
-template < typename _To, typename _From, typename = std::enable_if_t<!std::is_same<clean_type_t<_To>, clean_type_t<_From&>>::value>>
-FORCELINLINE _To fast_dynamic_cast(const _From& ref) {
-    using _ToPtr = std::add_pointer_t<std::remove_reference_t<_To>>;
-    auto casted_ptr = smart_cast<_ToPtr>(const_cast<_From*>(&ref));
-    assert(casted_ptr);
-    return *casted_ptr;
-};
-
-// std::dynamic_pointer_cast
-template < typename _To, typename _From >
-FORCELINLINE std::shared_ptr<_To> smart_pointer_cast(const std::shared_ptr<_From>& ptr) {
-    // Do not use std::move, since this might actually prevent RVO on MSVC 2015 and upwards
-    return std::shared_ptr<_To>(ptr, fast_dynamic_cast<_To*>(ptr.get()));
+    src_value_nocv &argument = const_cast<src_value_nocv &>(*src);
+    dest_value_nocv *result = smart_cast_helper<dest_value_nocv *, src_value_nocv, std::is_base_of_v<dest_value_nocv, src_value_nocv>>::cast(argument);
+    // add cv-qualifiers from src_value to the result and attempt to return it;
+    // this return statement will fail to compile if dest_type_cv_protected cannot be implicitly converted to dest_type,
+    // meaning that such a conversion would lose some of src_value's cv-qualifiers
+    return result;
 }
 
-// T -> T
-template < typename _To >
-FORCELINLINE _To smart_cast(_To ptr) { return ptr; }
+template <typename dest_type, typename src_type>
+inline typename std::enable_if_t<std::is_lvalue_reference_v<dest_type>, dest_type> smart_cast(src_type &&src) {
+    typedef typename std::remove_reference_t<dest_type> dest_value;
+    dest_value *result = smart_cast<dest_value *>(&src);
+    assert(result);
+    return *result;
+}
