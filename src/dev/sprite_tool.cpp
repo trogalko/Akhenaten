23c38f544a596756f3fdc802c849feb4dae0acf7
dalerank <dalerankn8@gmail.com>
2025-01-09 20:30:29 +0100

    dev: add sprite tool for ease investigating game resources

--- src/dev/sprite_tool.cpp
+++ src/dev/sprite_tool.cpp
@@ -22,19 +22,44 @@ void game_debug_show_properties_object(imagepak_handle ipak) {
         const auto &names = ipak.handle->names();
         const auto &ids = ipak.handle->image_ids();
         for (int i = 0, size = names.size(); i < size; ++i) {
-            ImGui::PushID(0x83000000 | i);
+            ImGui::PushID(0x83000000 + i);
 
             ImGui::TableNextRow();
             ImGui::TableSetColumnIndex(0);
             ImGui::AlignTextToFramePadding();
-            bool anim_open = ImGui::TreeNodeEx((void*)names[i].c_str(), ImGuiTreeNodeFlags_DefaultOpen, "%s", names[i].c_str());
+            const image_t* img = image_get(image_desc{ipak.id, i, 0});
+            bool anim_open = ImGui::TreeNodeEx(img, ImGuiTreeNodeFlags_DefaultOpen, "%s", img->bmp.name);
             ImGui::TableSetColumnIndex(1);
 
-            const int offset = ids[i];
+            const int ioffset = ids[i];
 
             if (anim_open) {
                 game_debug_show_property("id", i, true);
-                game_debug_show_property("offset", offset, true);
+                game_debug_show_property("i_offset", ioffset, true);
+
+                if (img && img->atlas.p_atlas) {
+                    game_debug_show_property("atlasname", img->bmp.name);
+                    const vec2i tx_offset = img->atlas.offset;
+
+                    ImGui::PushID(0x88000000 + i);
+                    ImGui::TableNextRow();
+                    ImGui::TableSetColumnIndex(0);
+                    ImGui::AlignTextToFramePadding();
+                    ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen | ImGuiTreeNodeFlags_Bullet;
+                    ImGui::TreeNodeEx("Field", flags, "Image");
+                    ImGui::TableSetColumnIndex(1);
+                    ImGui::SetNextItemWidth(-FLT_MIN);
+
+                    ImGui::InputInt2("Image", (int *)&tx_offset);
+
+                    const vec2i atlas_size(img->atlas.p_atlas->width, img->atlas.p_atlas->height);
+                    ImVec2 uv_min(tx_offset.x / (float)atlas_size.x, tx_offset.y / (float)atlas_size.y);
+                    ImVec2 uv_max((tx_offset.x + img->width) / (float)atlas_size.x, (tx_offset.y + img->height) / (float)atlas_size.y);
+                    ImGui::Image(img->atlas.p_atlas->texture, ImVec2(img->width, img->height), uv_min, uv_max);
+
+                    ImGui::NextColumn();
+                    ImGui::PopID();
+                }
 
                 ImGui::TreePop();
             }
