238ac665da9e02fe1f87f81acd0cd1353818cd37
dalerank <dalerankn8@gmail.com>
2025-01-23 00:23:39 +0100

    log: simplify function arguments

--- src/core/log.h
+++ src/core/log.h
@@ -3,64 +3,65 @@
 #include <SDL_log.h>
 
 #include <fstream>
+#include "core/xstring.h"
 
 namespace logs {
 /// Initialize SDL logger
 void initialize();
-void switch_output(const char *folder);
+void switch_output(pcstr folder);
 
 // Note: to handle format-security warnings
 template <class... Args>
-inline void critical(char const* format, Args... args) {
+inline void critical(pcstr format, Args... args) {
     SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void critical(char const* msg) {
+inline void critical(pcstr msg) {
     SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
 template <class... Args>
-inline void error(char const* format, Args... args) {
+inline void error(pcstr format, Args... args) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void error(char const* msg) {
+inline void error(pcstr msg) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
 template <class... Args>
-inline void warn(char const* format, Args... args) {
+inline void warn(pcstr format, Args... args) {
     SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void warn(char const* msg) {
+inline void warn(pcstr msg) {
     SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
 template <class... Args>
-inline void info(char const* format, Args... args) {
+inline void info(pcstr format, Args... args) {
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void info(char const* msg) {
+inline void info(pcstr msg) {
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
 template <class... Args>
-inline void debug(char const* format, Args... args) {
+inline void debug(pcstr format, Args... args) {
     SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void debug(char const* msg) {
+inline void debug(pcstr msg) {
     SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
 template <class... Args>
-inline void verbose(char const* format, Args... args) {
+inline void verbose(pcstr format, Args... args) {
     SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, format, args...);
 }
 
-inline void verbose(char const* msg) {
+inline void verbose(pcstr msg) {
     SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
 }
 
