575c0030dd503699aaac894e197a879ae2764da0
dalerank <dalerankn8@gmail.com>
2025-02-16 23:09:26 +0100

    figures: figure_scribe removed unused function

--- src/figuretype/figure_scriber.cpp
+++ src/figuretype/figure_scriber.cpp
@@ -1,14 +1,8 @@
 #include "figuretype/figure_scriber.h"
 
 #include "figure/service.h"
-#include "js/js_game.h"
 
 figures::model_t<figure_scriber> scriber_m;
-
-ANK_REGISTER_CONFIG_ITERATOR(config_load_figure_scriber);
-void config_load_figure_scriber() {
-    scriber_m.load();
-}
 
 void figure_scriber::figure_action() {
     switch (action_state()) {
@@ -34,8 +28,4 @@ void academy_coverage(building* b, figure *f, int&) {
 int figure_scriber::provide_service() {
     int houses_serviced = figure_provide_culture(tile(), &base, academy_coverage);
     return houses_serviced;
-}
-
-const animations_t &figure_scriber::anim() const {
-    return scriber_m.anim;
 }
